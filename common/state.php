5942f37da365e08c33ad2eab36924540e6f876c0
nefuddos <1791558967@qq.com>
2015-04-08 16:11:33 +0800

    用户在线状态信息文件,有待修正(目前只能显示单一用户)

--- common/state.php
+++ common/state.php
@@ -5,8 +5,7 @@ session_start();
  * 显示哪个用户在线,哪个用户不在线
  */
 $arr = $_SESSION;
-print_r($arr);
-echo '<br />' ;
-print_r(state());
-echo '<br />' ;
+foreach ( $arr as $value ) {
+    echo '<li>姓名:&nbsp'.$value .'</li>';
+}
 ?>
