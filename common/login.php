dba5e7a29b0926c9e4e9013d5d08dad0cab422f2
Jingui Ren <nefu123ddos@gmail.com>
2015-04-12 16:08:19 +0800

    多账号登录
    解决同一浏览器下多账号登录问题！

--- common/login.php
+++ common/login.php
@@ -10,15 +10,21 @@
         $pass = $_POST["pass"];
         //$name = "rjg";
         //$pass = "123";
+        //如果当前的session_id有指定的名字，则应该将指定的用户设置为失效状态.然后更新$session_id的值
+        $id = session_id();
+        if($_SESSION[$id])
+        {
+            updateState($_SESSION[$id],time()-2000);
+            $_SESSION[$id] = $name;
+        }
+        if (!isset($_SESSION[$id])) {
+            $_SESSION[$id] = $name;
+        }
         //获取客户端ip
          $ipifo = getip();
         if(AccountXML ( $name , $pass ,$ipifo,time()))
         {
             $array_ifo["tag"] = true;
-            $id = session_id();
-            if (!isset($_SESSION[$id])) {
-                $_SESSION[$id] = $name;
-              }
         }  else {
             $array_ifo["tag"] = false;
         }
